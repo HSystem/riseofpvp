9a35b1c09463c6f1dea5cad77f25b4f1c1bd66e4
BuildTools <unconfigured@null.spigotmc.org>
2021-01-01 22:47:56 +0200

    added Welcome-plugin; +small tweaks

---
+++ welcome/src/main/java/fi/heikkitoivanen/riseofpvp/Welcome.java
@@ -0,0 +1,32 @@
+package fi.heikkitoivanen.riseofpvp;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Level;
+
+public class Welcome extends JavaPlugin implements Listener {
+    @Override
+    public void onEnable() {
+        this.getServer().getPluginManager().registerEvents(this, this);
+        getLogger().log(Level.INFO,"Plugin Enabled");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().log(Level.INFO,"Plugin Disabled");
+    }
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event) {
+        event.getPlayer().sendMessage(ChatColor.LIGHT_PURPLE+"Welcome to the Rise of PVP!! Who will be the winner?");
+    }
+
+
+
+}
